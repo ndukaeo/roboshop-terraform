5dcbf98286449c36570e3ce8cc6dcfdfdcc4b7b2
nduka ojeh <nojeh@baneollc.com>
2024-07-28 16:27:32 -0400

    create subnets for roboshop

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,3 +6,42 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zones = var.availability_zones[count.index]
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  availability_zones = var.availability_zones[count.index]
+  tags = {
+    Name = "web_subnet"
+  }
+}
+
+resource "aws_subnet" "app" {
+  count      = length(var.app_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  availability_zones = var.availability_zones[count.index]
+  tags = {
+    Name = "app_subnet"
+  }
+}
+
+resource "aws_subnet" "db" {
+  count      = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  availability_zones = var.availability_zones[count.index]
+  tags = {
+    Name = "db_subnet"
+  }
+}
