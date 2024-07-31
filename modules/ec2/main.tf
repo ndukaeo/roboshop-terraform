4d0777e9a758c630034972822257fced19d7cfd4
nduka ojeh <nojeh@baneollc.com>
2024-07-30 22:20:19 -0400

    adding load balancer for public

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -172,6 +172,14 @@ resource "aws_lb_listener" "main" {
 }
 
 
+resource "aws_route53_record" "lb" {
+  count    = var.asg ? 1 : 0
+  zone_id = var.zone_id
+  name    = "${var.name}.${var.env}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_lb.main.*.dns_name[count.index]]
+}
 
 
 
