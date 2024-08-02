99cb3784f5f50aebde5b7c49032eddcdf99d66e8
nduka ojeh <nojeh@baneollc.com>
2024-08-02 10:08:04 -0400

    asg module added

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -142,7 +142,7 @@ resource "aws_lb_listener" "public-https" {
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  sl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.acm_https_arn
 
   default_action {
