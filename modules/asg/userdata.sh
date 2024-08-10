7c777c553b3a68cf730424a8c1ea90860474a5b8
nduka ojeh <nojeh@baneollc.com>
2024-08-09 20:05:21 -0400

    install docker in al app servers

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+dnf install docker -y
+
+
 #
 #pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
 #ansible-pull -i localhost, -U https://github.com/ndukaeo/roboshop-ansible main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
 #
 #
-dnf install docker -y
+
 
 
