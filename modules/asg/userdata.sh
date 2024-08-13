18ff11c35762695ef278fbaf83c2279ad593c1c6
nduka ojeh <nojeh@baneollc.com>
2024-08-13 18:25:34 -0400

    terraform with containers

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -6,11 +6,11 @@ growpart /dev/nvme0n1 4
 
 lvextend -r -L +10G /dev/mapper/RootVG-homeVol
 
-#
-#pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
+pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
+
 #ansible-pull -i localhost, -U https://github.com/ndukaeo/roboshop-ansible main.yml -e env=${env} -e role_name=${role_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
-#
-#
+
+ansible-pull -i localhost, -U https://github.com/ndukaeo/roboshop-ansible main.yml -e env=${env} -e role_name=docker -e app_name=${app_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
 
 
 
